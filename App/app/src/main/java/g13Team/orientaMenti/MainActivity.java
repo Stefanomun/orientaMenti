414fe60d093d55ffc112b0d96bec5d0d8f33cb1c
Stefanomun <81584446+Stefanomun@users.noreply.github.com>
2022-06-22 17:32:09 +0200

    Aggiunta e collegamento pagina login

--- App/app/src/main/java/g13Team/orientaMenti/MainActivity.java
+++ App/app/src/main/java/g13Team/orientaMenti/MainActivity.java
@@ -2,8 +2,10 @@ package g13Team.orientaMenti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
@@ -13,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void chiamaLogin(View view){
+        Intent login = new Intent(MainActivity.this, Login.class);
+        startActivity(login);
     }
 }

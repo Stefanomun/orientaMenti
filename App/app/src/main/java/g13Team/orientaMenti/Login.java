79395dc964a398d170b87f1ce928151735687f7c
Stefanomun <81584446+Stefanomun@users.noreply.github.com>
2022-06-22 18:48:42 +0200

    Completamento pagina login e collegamento indietro a pagina iniziale

--- App/app/src/main/java/g13Team/orientaMenti/Login.java
+++ App/app/src/main/java/g13Team/orientaMenti/Login.java
@@ -2,7 +2,9 @@ package g13Team.orientaMenti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void indietro(View view){
+        Intent indietro = new Intent(Login.this, MainActivity.class);
+        startActivity(indietro);
     }
 }

fc0218e44a46323b44f1b9748abe4215b24af4e5
Stefanomun <81584446+Stefanomun@users.noreply.github.com>
2022-06-22 20:37:49 +0200

    Gestione accessi e costruzione layout pagina errore login

--- App/app/src/main/java/g13Team/orientaMenti/Login.java
+++ App/app/src/main/java/g13Team/orientaMenti/Login.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
 
@@ -17,5 +19,28 @@ public class Login extends AppCompatActivity {
     public void indietro(View view){
         Intent indietro = new Intent(Login.this, MainActivity.class);
         startActivity(indietro);
+    }
+
+    public void accesso(View view){
+        EditText editEmail = (EditText) findViewById(R.id.mail);
+        String email = editEmail.getText().toString();
+        EditText editPassword = (EditText) findViewById(R.id.password);
+        String password = editPassword.getText().toString();
+        if(email.equals("marta.rossi@studenti.it") && password.equals("studente")){
+            Intent accesso = new Intent(Login.this, DashboardStudente.class);
+            startActivity(accesso);
+        }
+        else if(email.equals("paolo.bianchi@docenti.it") && password.equals("docente")){
+            Intent accesso = new Intent(Login.this, DashboardDocente.class);
+            startActivity(accesso);
+        }
+        else if(email.equals("marco.verdi@manager.it") && password.equals("manager")){
+            Intent accesso = new Intent(Login.this, DashboardHR.class);
+            startActivity(accesso);
+        }
+        else{
+            Intent accesso = new Intent(Login.this, LoginError.class);
+            startActivity(accesso);
+        }
     }
 }

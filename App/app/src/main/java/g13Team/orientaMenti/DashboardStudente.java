d78f51c0008ca1df247f2a301b73c7cb2fb04e30
Sabato Genovese <sabatinogenovese51@gmail.com>
2022-06-23 20:57:37 +0200

    Aggiunta menu

--- App/app/src/main/java/g13Team/orientaMenti/DashboardStudente.java
+++ App/app/src/main/java/g13Team/orientaMenti/DashboardStudente.java
@@ -1,21 +1,37 @@
 package g13Team.orientaMenti;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 
 public class DashboardStudente extends AppCompatActivity {
 
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_studente);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     public void listaAziende(View view){
         Intent lista = new Intent(DashboardStudente.this, ListaAziende.class);
         startActivity(lista);
+    }
+
+   @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public void menu(View view) {
+        toolbar.showOverflowMenu();
     }
 }

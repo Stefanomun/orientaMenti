5850350f00e8c6cc209da6c97034fd5d6feaffed
Stefanomun <81584446+Stefanomun@users.noreply.github.com>
2022-06-23 13:46:16 +0200

    Risolto problema lista aziende

--- App/app/src/main/java/g13Team/orientaMenti/DashboardStudente.java
+++ App/app/src/main/java/g13Team/orientaMenti/DashboardStudente.java
@@ -2,7 +2,9 @@ package g13Team.orientaMenti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class DashboardStudente extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class DashboardStudente extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_studente);
+    }
+
+    public void listaAziende(View view){
+        Intent lista = new Intent(DashboardStudente.this, ListaAziende.class);
+        startActivity(lista);
     }
 }
--- App/app/src/main/java/g13Team/orientaMenti/ListaAziende.java
+++ App/app/src/main/java/g13Team/orientaMenti/ListaAziende.java
@@ -1,4 +1,21 @@
 package g13Team.orientaMenti;
 
-public class ListaAziende {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class ListaAziende extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_aziende);
+    }
+
+    public void indietro(View view){
+        Intent indietro = new Intent(ListaAziende.this, DashboardStudente.class);
+        startActivity(indietro);
+    }
 }

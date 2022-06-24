6a95fb1d2c0031a1ba05780648a29af1a621f297
Antonio Sellitto <antoniosellitto00@gmail.com>
2022-06-24 17:55:31 +0200

    Lista Bandi azienda

---
+++ App/app/src/main/java/g13Team/orientaMenti/ListaBandiAzienda2.java
@@ -0,0 +1,14 @@
+package g13Team.orientaMenti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaBandiAzienda2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_bandi_azienda2);
+    }
+}

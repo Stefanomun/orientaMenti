b5a78bdc5fdc333154987640467c70eefe3e1005
Sabato Genovese <sabatinogenovese51@gmail.com>
2022-06-24 11:52:29 +0200

    Aggiunta menu in ListaAziende

--- App/app/src/main/java/g13Team/orientaMenti/DashboardStudente.java
+++ App/app/src/main/java/g13Team/orientaMenti/DashboardStudente.java
@@ -6,11 +6,14 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class DashboardStudente extends AppCompatActivity {
 
-    Toolbar toolbar;
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,11 +30,29 @@ public class DashboardStudente extends AppCompatActivity {
 
    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
+        getMenuInflater().inflate(R.menu.menu_studente, menu);
         return true;
     }
 
     public void menu(View view) {
         toolbar.showOverflowMenu();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.messaggi:
+                Toast.makeText(this, "Messaggi", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.profilo:
+                Toast.makeText(this, "Profilo", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.logout:
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
--- App/app/src/main/java/g13Team/orientaMenti/ListaAziende.java
+++ App/app/src/main/java/g13Team/orientaMenti/ListaAziende.java
@@ -1,21 +1,57 @@
 package g13Team.orientaMenti;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class ListaAziende extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_aziende);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     public void indietro(View view){
         Intent indietro = new Intent(ListaAziende.this, DashboardStudente.class);
         startActivity(indietro);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_studente, menu);
+        return true;
+    }
+
+    public void menu(View view) {
+        toolbar.showOverflowMenu();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.messaggi:
+                Toast.makeText(this, "Messaggi", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.profilo:
+                Toast.makeText(this, "Profilo", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.logout:
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }

3f6233ab5711fa2cf105c65280346c2f9fa6cdf6
Antonio Sellitto <antoniosellitto00@gmail.com>
2022-06-26 01:57:45 +0200

    Lista Studenti Docente

---
+++ App/app/src/main/java/g13Team/orientaMenti/docente/ListaStudentiDocente.java
@@ -0,0 +1,14 @@
+package g13Team.orientaMenti.docente;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaStudentiDocente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_studenti_docente);
+    }
+}

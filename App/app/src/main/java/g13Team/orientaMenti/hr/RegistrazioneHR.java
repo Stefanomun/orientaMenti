51756acecf31a4f79b56e7629d1adce2869e6fa8
kenz097 <81226666+AngelaDeM@users.noreply.github.com>
2022-06-25 23:06:34 +0200

    Realizzazione Activity RegistrazioneHR

---
+++ App/app/src/main/java/g13Team/orientaMenti/hr/RegistrazioneHR.java
@@ -0,0 +1,15 @@
+package g13Team.orientaMenti.hr;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import g13Team.orientaMenti.R;
+
+public class RegistrazioneHR extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrazione_hr);
+    }
+}

e095c81a78ed59ff78fe3f0fbe1f58440799f108
Antonio Sellitto <antoniosellitto00@gmail.com>
2022-06-28 21:37:51 +0200

    Backend per accedere alle liste studenti dalle dashboard docente e hr

--- App/app/src/main/java/g13Team/orientaMenti/docente/DashboardDocente.java
+++ App/app/src/main/java/g13Team/orientaMenti/docente/DashboardDocente.java
@@ -1,8 +1,13 @@
 package g13Team.orientaMenti.docente;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import g13Team.orientaMenti.R;
 import g13Team.orientaMenti.menu.MenuDocenteHR;
+import g13Team.orientaMenti.studente.DashboardStudente;
+import g13Team.orientaMenti.studente.ListaAziende;
 
 public class DashboardDocente extends MenuDocenteHR {
 
@@ -12,5 +17,10 @@ public class DashboardDocente extends MenuDocenteHR {
         setContentView(R.layout.activity_dashboard_docente);
 
         super.create(this);
+    }
+
+    public void listaStudenti(View view){
+        Intent intent = new Intent(DashboardDocente.this, ListaStudentiDocente.class);
+        startActivity(intent);
     }
 }
--- App/app/src/main/java/g13Team/orientaMenti/hr/DashboardHR.java
+++ App/app/src/main/java/g13Team/orientaMenti/hr/DashboardHR.java
@@ -1,8 +1,12 @@
 package g13Team.orientaMenti.hr;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import g13Team.orientaMenti.R;
+import g13Team.orientaMenti.docente.DashboardDocente;
+import g13Team.orientaMenti.docente.ListaStudentiDocente;
 import g13Team.orientaMenti.menu.MenuDocenteHR;
 
 public class DashboardHR extends MenuDocenteHR {
@@ -13,5 +17,10 @@ public class DashboardHR extends MenuDocenteHR {
         setContentView(R.layout.activity_dashboard_hr);
 
         super.create(this);
+    }
+
+    public void listaStudenti(View view){
+        Intent intent = new Intent(DashboardHR.this, ListaStudentiHR.class);
+        startActivity(intent);
     }
 }

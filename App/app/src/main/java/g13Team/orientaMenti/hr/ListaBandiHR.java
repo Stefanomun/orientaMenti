cc746a39068f5c5fc0792a56b2c84d318e415fae
Antonio Sellitto <antoniosellitto00@gmail.com>
2022-06-28 22:32:03 +0200

    creazione della ListaBandiHR e creazione backend del collegamento con la DashboardHR

--- App/app/src/main/java/g13Team/orientaMenti/hr/ListaBandiHR.java
+++ App/app/src/main/java/g13Team/orientaMenti/hr/ListaBandiHR.java
@@ -3,6 +3,9 @@ package g13Team.orientaMenti.hr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import g13Team.orientaMenti.R;
 
 public class ListaBandiHR extends AppCompatActivity {
 
@@ -11,4 +14,8 @@ public class ListaBandiHR extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_bandi_hr);
     }
+
+    /*public void aggiungiBando(View view){
+
+    }*/
 }

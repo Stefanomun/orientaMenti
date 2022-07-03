a8bc90cc5899a35a430e054aaba90dce492be775
Sabato Genovese <sabatinogenovese51@gmail.com>
2022-07-04 00:37:57 +0200

    continuo sviluppo filtri

--- App/app/src/main/java/g13Team/orientaMenti/ListaBandiAzienda2.java
+++ App/app/src/main/java/g13Team/orientaMenti/ListaBandiAzienda2.java
@@ -1,7 +1,5 @@
 package g13Team.orientaMenti;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,30 +7,33 @@ import android.view.View;
 import g13Team.orientaMenti.menu.MenuStudente;
 import g13Team.orientaMenti.studente.BandoCrewMember;
 import g13Team.orientaMenti.studente.DashboardStudente;
-import g13Team.orientaMenti.studente.DettaglioIntesa;
 import g13Team.orientaMenti.studente.DettaglioLidl;
-import g13Team.orientaMenti.studente.ListaAziende;
 
 public class ListaBandiAzienda2 extends MenuStudente {
+
+    private boolean indietroAlternativo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_bandi_azienda2);
         super.create(this);
+        indietroAlternativo = getIntent().getBooleanExtra("indietroAlternativo", true);
     }
 
-    public void indietro(View view){
-        Intent indietro = new Intent(ListaBandiAzienda2.this, DettaglioLidl.class);
-        startActivity(indietro);
+    public void indietro(View view) {
+        if (indietroAlternativo)
+            startActivity(new Intent(ListaBandiAzienda2.this, ListaBandi.class));
+        else
+            startActivity(new Intent(ListaBandiAzienda2.this, DettaglioLidl.class));
     }
 
-    public void home(View view){
+    public void home(View view) {
         Intent home = new Intent(ListaBandiAzienda2.this, DashboardStudente.class);
         startActivity(home);
     }
 
-    public void bando1(View view){
+    public void bando1(View view) {
         Intent home = new Intent(ListaBandiAzienda2.this, BandoCrewMember.class);
         home.putExtra("bando", "lidl");
         startActivity(home);
--- App/app/src/main/java/g13Team/orientaMenti/ListaBandiAzienda3.java
+++ App/app/src/main/java/g13Team/orientaMenti/ListaBandiAzienda3.java
@@ -1,7 +1,5 @@
 package g13Team.orientaMenti;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,28 +8,32 @@ import g13Team.orientaMenti.menu.MenuStudente;
 import g13Team.orientaMenti.studente.BandoConsulenteAziendale;
 import g13Team.orientaMenti.studente.DashboardStudente;
 import g13Team.orientaMenti.studente.DettaglioIntesa;
-import g13Team.orientaMenti.studente.ListaAziende;
 
 public class ListaBandiAzienda3 extends MenuStudente {
+
+    private boolean indietroAlternativo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_bandi_azienda3);
         super.create(this);
+        indietroAlternativo = getIntent().getBooleanExtra("indietroAlternativo", true);
     }
 
-    public void indietro(View view){
-        Intent indietro = new Intent(ListaBandiAzienda3.this, DettaglioIntesa.class);
-        startActivity(indietro);
+    public void indietro(View view) {
+        if (indietroAlternativo)
+            startActivity(new Intent(ListaBandiAzienda3.this, ListaBandi.class));
+        else
+            startActivity(new Intent(ListaBandiAzienda3.this, DettaglioIntesa.class));
     }
 
-    public void home(View view){
+    public void home(View view) {
         Intent home = new Intent(ListaBandiAzienda3.this, DashboardStudente.class);
         startActivity(home);
     }
 
-    public void bando1(View view){
+    public void bando1(View view) {
         Intent home = new Intent(ListaBandiAzienda3.this, BandoConsulenteAziendale.class);
         home.putExtra("bando", "intesa");
         startActivity(home);
